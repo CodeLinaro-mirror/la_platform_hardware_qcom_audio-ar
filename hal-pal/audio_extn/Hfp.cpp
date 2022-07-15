0d93d4baa8219ee1e0b3c20b9ae48e3a4fb6a212
Sitong <quic_sitochen@quicinc.com>
2022-07-15 16:47:54 +0800

    audio-hal-ar: change HFP uplink device to handset_mic
    Use same pal device for HFP uplink and other primary zone capture cases,
    to avoid unsuported and unneccessary switching between different pal devices
    with common TDM interface.
    
    Change-Id:I9b8d82e6e4d738b1c8dff60c62c0529e4f717356

--- hal-pal/audio_extn/Hfp.cpp
+++ hal-pal/audio_extn/Hfp.cpp
@@ -288,7 +288,7 @@ static int32_t start_hfp(std::shared_ptr<AudioDevice> adev __unused,
     devices[0].config.ch_info = ch_info;
     devices[0].config.aud_fmt_id = PAL_AUDIO_FMT_PCM_S16_LE;
 
-    devices[1].id = PAL_DEVICE_IN_SPEAKER_MIC;
+    devices[1].id = PAL_DEVICE_IN_HANDSET_MIC;
 
     ret = pal_stream_open(&stream_tx_attr,
             no_of_devices, devices,
