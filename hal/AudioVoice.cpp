db96dc0e44a280b02008da6b79f40694be97f1e9
Kunlei Zhang <quic_kunleiz@quicinc.com>
2023-09-18 20:49:15 +0800

    HAL: default initialize crsCall flag
    Add default initialization for crsCall flag
    
    
    Change-Id: I20ff446390624b1d1b7f5b5ce56a284f138aff79
    (cherry picked from commit ad51d04ad5858edbd4a521662f56d8adc0bc2083)

--- hal/AudioVoice.cpp
+++ hal/AudioVoice.cpp
@@ -28,7 +28,7 @@
  *
  * Changes from Qualcomm Innovation Center are provided under the following license:
  *
- * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
@@ -1586,6 +1586,7 @@ AudioVoice::AudioVoice() {
     voice_.session[MMODE2_SESS_IDX].vsid = VOICEMMODE2_VSID;
     voice_.crsVsid = 0;
     voice_.crsVol = 0.4;
+    voice_.crsCall = false;
     voice_.crsLoopback = true;
     stream_out_primary_ = NULL;
 }
