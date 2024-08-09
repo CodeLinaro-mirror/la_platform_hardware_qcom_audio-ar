44150697eebae7c3f7365db8cfbcf690b33a8589
Kunlei Zhang <quic_kunleiz@quicinc.com>
2024-08-09 15:51:36 +0800

    hal: core: reset crs device if headset connection change
    Reset crs device if headset connection change.
    
    Change-Id: I834cd510839bbd4f56ab7705d275a53e2eda89b7
    (cherry picked from commit a94ec80218ea5bdc9b9d1d87f1166312614eaf74)

--- hal/core/Telephony.cpp
+++ hal/core/Telephony.cpp
@@ -292,11 +292,9 @@ void Telephony::onExternalDeviceConnectionChanged(const AudioDevice& extDevice,
         mTxDevice = getMatchingTxDevice(mRxDevice);
         updateDevices();
     } else {
-        if (mIsCRSStarted && !hasValidPlaybackStream) {
-            mRxDevice = kDefaultCRSRxDevice;
-            mTxDevice = getMatchingTxDevice(mRxDevice);
-            updateDevices();
-        }
+        mRxDevice = kDefaultRxDevice;
+        mTxDevice = getMatchingTxDevice(mRxDevice);
+        updateDevices();
     }
 
 }
@@ -799,7 +797,8 @@ void Telephony::stopCall() {
         updateVoiceMetadataForBT(false);
     }
     mPalHandle = nullptr;
-    if (mSetUpdates.mIsCrsCall) {
+    if (mSetUpdates.mIsCrsCall &&
+        mRxDevice.type.type == AudioDeviceType::OUT_SPEAKER) {
         mRxDevice = kDefaultRxDevice;
         mTxDevice = getMatchingTxDevice(mRxDevice);
     }
