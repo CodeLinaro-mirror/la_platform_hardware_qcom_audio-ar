ec4c88e8b4b566f8c7cd177e16a70abd63976e5f
Swapnil Kangralkar(Temp) <quic_skangral@quicinc.com>
2024-08-30 22:02:21 +0530

    hal: Update vendor_audio_interface.xml
    Remove hal vendor extension service registration.
    Ihaladapter property is added as system external property,
    so remove from vendor property.
    
    Change-Id: I821134e6d99ff3f4a109871d45c0a226e8be8b1f
    (cherry picked from commit 6d950910ea99a0553759253f1d177bc6336ae8d9)

--- configs/monaco/audio-properties.mk
+++ configs/monaco/audio-properties.mk
@@ -2,10 +2,6 @@
 AUDIO_HAL_PROP += \
 vendor.audio.hdr.spf.record.enable=false
 
-# ihaladaptervendorextension_enable for the framework to bind to this service
-AUDIO_HAL_PROP += \
-ro.audio.ihaladaptervendorextension_enable=true
-
 # spf hdr record either true or false
 AUDIO_HAL_PROP += \
 vendor.audio.hdr.record.enable=false
@@ -19,7 +15,6 @@ AUDIO_HAL_PROP += \
 vendor.audio.compress_capture.enabled=true \
 vendor.audio.compress_capture.aac=true
 # compress capture end
-
 
 PRODUCT_VENDOR_PROPERTIES += \
     $(AUDIO_HAL_PROP)
