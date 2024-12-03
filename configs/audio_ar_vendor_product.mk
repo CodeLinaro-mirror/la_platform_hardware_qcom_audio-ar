273ee2038a5153b288d2331632c413b9face5433
Raghu Ballappa Bankapur <quic_rbankapu@quicinc.com>
2024-12-03 17:05:06 +0530

    AHAL: Creates device configuration for msmnile_au
    Creates respective xml files and msmnile_au.mk file
    for the device configuration of msmnile_au
    
    Change-Id: I5eab267883c83b57544ea04cedc0393c8689858c
    Signed-off-by: Raghu Ballappa Bankapur <quic_rbankapu@quicinc.com>

--- configs/audio_ar_vendor_product.mk
+++ configs/audio_ar_vendor_product.mk
@@ -21,7 +21,12 @@ $(error AUDIO_USE_STUB_HAL is $(AUDIO_USE_STUB_HAL))
 -include $(TOPDIR)vendor/qcom/opensource/audio-hal/primary-hal/configs/common/default.mk
 else
 #$(error TARGET_BOARD_PLATFORM is $(TARGET_BOARD_PLATFORM))
+ifeq ($(TARGET_BOARD_PLATFORM)$(TARGET_BOARD_SUFFIX), gen4_gvm)
 include vendor/qcom/opensource/audio-hal/primary-hal/configs/gen4_au/gen4_au.mk
+endif
+ifeq ($(TARGET_BOARD_PLATFORM)$(TARGET_BOARD_SUFFIX), msmnile_gvmq)
+include vendor/qcom/opensource/audio-hal/primary-hal/configs/msmnile_gvmq/msmnile_gvmq.mk
+endif
 endif
 
 $(warning audio check QC_HWASAN: $(QC_HWASAN) sanitize_target $(SANITIZE_TARGET))
---
+++ configs/msmnile_gvmq/audio-modules.mk
@@ -0,0 +1,105 @@
+ifeq ($(TARGET_USES_QMAA),true)
+    ifneq ($(TARGET_USES_QMAA_OVERRIDE_AUDIO),true)
+    #QMAA Mode is enabled
+    TARGET_IS_HEADLESS := true
+    endif
+endif
+
+#Packages that should not be installed in QMAA are enabled here
+ifneq ($(TARGET_IS_HEADLESS),true)
+
+#AGM
+AUDIO_AGM := libagmclient
+AUDIO_AGM += libagmipcservice
+AUDIO_AGM += libagm
+AUDIO_AGM += agmplay
+AUDIO_AGM += agmcap
+AUDIO_AGM += libagmmixer
+AUDIO_AGM += agmcompressplay
+AUDIO_AGM += libagm_mixer_plugin
+AUDIO_AGM += libagm_pcm_plugin
+AUDIO_AGM += libagm_compress_plugin
+AUDIO_AGM += agmcompresscap
+AUDIO_AGM += agmvoiceui
+AUDIO_AGM += agmhostless
+
+#PAL Module
+AUDIO_PAL := libar-pal
+AUDIO_PAL += lib_bt_bundle
+AUDIO_PAL += lib_bt_aptx
+AUDIO_PAL += lib_bt_ble
+AUDIO_PAL += catf
+AUDIO_PAL += PalTest
+AUDIO_PAL += libaudiochargerlistener
+AUDIO_PAL += libhfp_pal
+#PAL Service
+AUDIO_PAL += libpalclient
+AUDIO_PAL += libpalipcservice
+#PAL Stream Modules
+AUDIO_PAL += libstream_compress
+AUDIO_PAL += libstream_incall
+AUDIO_PAL += libstream_commonproxy
+AUDIO_PAL += libstream_contextproxy
+AUDIO_PAL += libstream_sensorpcmdata
+AUDIO_PAL += libstream_sensorrenderer
+AUDIO_PAL += libstream_ultrasound
+AUDIO_PAL += libstream_pcm
+AUDIO_PAL += libstream_haptics
+AUDIO_PAL += libstream_acd
+AUDIO_PAL += libstream_nontunnel
+AUDIO_PAL += libstream_soundtrigger
+AUDIO_PAL += libstream_acdb
+#PAL Sessions Modules
+AUDIO_PAL += libsession_agm
+AUDIO_PAL += libsession_pcm
+AUDIO_PAL += libsession_compress
+AUDIO_PAL += libsession_voice
+#PAL Device Modules
+AUDIO_PAL += libdev_bt
+AUDIO_PAL += libdev_display
+AUDIO_PAL += libdev_dummy
+AUDIO_PAL += libdev_ec_ref
+AUDIO_PAL += libdev_ext_ec
+AUDIO_PAL += libdev_fm
+AUDIO_PAL += libdev_handset
+AUDIO_PAL += libdev_handset_mic
+AUDIO_PAL += libdev_handset_va
+AUDIO_PAL += libdev_haptics
+AUDIO_PAL += libdev_headphone
+AUDIO_PAL += libdev_headset_mic
+AUDIO_PAL += libdev_headset_va
+AUDIO_PAL += libdev_proxy
+AUDIO_PAL += libdev_speaker
+AUDIO_PAL += libdev_speaker_mic
+AUDIO_PAL += libdev_ultrasound
+AUDIO_PAL += libdev_usb
+
+AUDIO_ACDB := workspaceFileXml.qwsp
+AUDIO_ACDB += acdb_cal.acdb
+AUDIO_ACDB += acdb_cal.acdbdelta
+
+AUDIO_MODULES := $(AUDIO_AGM)
+AUDIO_MODULES += $(AUDIO_PAL)
+AUDIO_MODULES += $(AUDIO_ACDB)
+
+ # sound trigger aidl library
+#AUDIO_MODULES += libsoundtriggerhal.qti
+
+# enable Listen Sound Model aidl 1.0
+#AUDIO_MODULES += \
+    liblistensoundmodelaidl
+
+# AIDL Audio modules
+
+AUDIO_MODULES += \
+    audiohalservice.qti \
+    libaudiocorehal.qti \
+    libaudiocorehal.default \
+    libaudioeffecthal.qti
+
+# add modules for fuzzing
+ifneq ($(filter audio,$(QC_HWASAN))$(filter hwaddress,$(SANITIZE_TARGET)),)
+AUDIO_MODULES += fuzz-audio-hal
+endif
+
+endif
