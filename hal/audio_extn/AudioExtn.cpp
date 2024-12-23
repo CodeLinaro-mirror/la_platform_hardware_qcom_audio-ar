AOSPA/android_hardware_qcom_audio

hal: BLE fixes for music and call transitions.
Send SourceMetadata if call is already started
and BLE device is connected during call active
state

Fix Metadata issue from framework where metadata
for voice call is coming after call becomes
inactive

Synchronize the voice start and suspend callback
from Bt offload library.

Change-Id: I46af9da444cba69a59486a5a77aff2d5e1fe335e

hal/audio_extn/AudioExtn.cpp
@@ -75,6 +75,8 @@ static batt_prop_is_charging_t batt_prop_is_charging;
 static bool battery_listener_enabled;
 static void *batt_listener_lib_handle;
 static bool audio_extn_kpi_optimize_feature_enabled = false;
+//TODO make this mutex part of class
+std::mutex reconfig_wait_mutex_;
 
 int AudioExtn::audio_extn_parse_compress_metadata(struct audio_config *config_, pal_snd_dec_t *pal_snd_dec,
                                str_parms *parms, uint32_t *sr, uint16_t *ch, bool *isCompressMetadataAvail) {
@@ -522,32 +524,43 @@ static int reconfig_cb (tSESSION_TYPE session_type, int state)
 {
     int ret = 0;
     pal_param_bta2dp_t param_bt_a2dp;
+    AHAL_DBG("reconfig_cb enter");
     if (session_type == LE_AUDIO_HARDWARE_OFFLOAD_ENCODING_DATAPATH) {
 
         /* If reconfiguration is in progress state, we do a2dp suspend.
          * If reconfiguration is in complete state, we do a2dp resume.
          */
         if (state == 0) {
+            std::unique_lock<std::mutex> guard(reconfig_wait_mutex_);
             param_bt_a2dp.a2dp_suspended = true;
+            param_bt_a2dp.dev_id = PAL_DEVICE_OUT_BLUETOOTH_BLE;
+
+            ret = pal_set_param(PAL_PARAM_ID_BT_A2DP_SUSPENDED, (void *)&param_bt_a2dp,
+                                sizeof(pal_param_bta2dp_t));
         } else if (state == 1) {
             param_bt_a2dp.a2dp_suspended = false;
-        }
-        param_bt_a2dp.dev_id = PAL_DEVICE_OUT_BLUETOOTH_BLE;
+            param_bt_a2dp.dev_id = PAL_DEVICE_OUT_BLUETOOTH_BLE;
 
-        ret = pal_set_param(PAL_PARAM_ID_BT_A2DP_SUSPENDED, (void *)&param_bt_a2dp,
-                            sizeof(pal_param_bta2dp_t));
+            ret = pal_set_param(PAL_PARAM_ID_BT_A2DP_SUSPENDED, (void *)&param_bt_a2dp,
+                                sizeof(pal_param_bta2dp_t));
+        }
     } else if (session_type == LE_AUDIO_HARDWARE_OFFLOAD_DECODING_DATAPATH) {
         if (state == 0) {
+            std::unique_lock<std::mutex> guard(reconfig_wait_mutex_);
             param_bt_a2dp.a2dp_capture_suspended = true;
+            param_bt_a2dp.dev_id = PAL_DEVICE_IN_BLUETOOTH_BLE;
+
+            ret = pal_set_param(PAL_PARAM_ID_BT_A2DP_CAPTURE_SUSPENDED, (void *)&param_bt_a2dp,
+                                sizeof(pal_param_bta2dp_t));
         } else if (state == 1) {
             param_bt_a2dp.a2dp_capture_suspended = false;
+            param_bt_a2dp.dev_id = PAL_DEVICE_IN_BLUETOOTH_BLE;
+
+            ret = pal_set_param(PAL_PARAM_ID_BT_A2DP_CAPTURE_SUSPENDED, (void *)&param_bt_a2dp,
+                                sizeof(pal_param_bta2dp_t));
         }
-        param_bt_a2dp.dev_id = PAL_DEVICE_IN_BLUETOOTH_BLE;
-
-        ret = pal_set_param(PAL_PARAM_ID_BT_A2DP_CAPTURE_SUSPENDED, (void *)&param_bt_a2dp,
-                            sizeof(pal_param_bta2dp_t));
     }
-
+    AHAL_ERR("reconfig_cb exit");
     return ret;
 }
 

CodeLinaro-mirror/la_platform_hardware_qcom_audio-ar

hal/audio_extn/AudioExtn.cpp
@@ -110,6 +110,8 @@ static batt_prop_is_charging_t batt_prop_is_charging;
 static bool battery_listener_enabled;
 static void *batt_listener_lib_handle;
 static bool audio_extn_kpi_optimize_feature_enabled = false;
+//TODO make this mutex part of class
+std::mutex reconfig_wait_mutex_;
 
 std::atomic<bool> AudioExtn::sServicesRegistered = false;
 
@@ -562,32 +564,43 @@ static int reconfig_cb (tSESSION_TYPE session_type, int state)
 {
     int ret = 0;
     pal_param_bta2dp_t param_bt_a2dp;
+    AHAL_VERBOSE("reconfig_cb enter");
     if (session_type == LE_AUDIO_HARDWARE_OFFLOAD_ENCODING_DATAPATH) {
 
         /* If reconfiguration is in progress state, we do a2dp suspend.
          * If reconfiguration is in complete state, we do a2dp resume.
          */
         if (state == 0) {
+            std::unique_lock<std::mutex> guard(reconfig_wait_mutex_);
             param_bt_a2dp.a2dp_suspended = true;
+            param_bt_a2dp.dev_id = PAL_DEVICE_OUT_BLUETOOTH_BLE;
+
+            ret = pal_set_param(PAL_PARAM_ID_BT_A2DP_SUSPENDED, (void *)&param_bt_a2dp,
+                                sizeof(pal_param_bta2dp_t));
         } else if (state == 1) {
             param_bt_a2dp.a2dp_suspended = false;
-        }
-        param_bt_a2dp.dev_id = PAL_DEVICE_OUT_BLUETOOTH_BLE;
+            param_bt_a2dp.dev_id = PAL_DEVICE_OUT_BLUETOOTH_BLE;
 
-        ret = pal_set_param(PAL_PARAM_ID_BT_A2DP_SUSPENDED, (void *)&param_bt_a2dp,
-                            sizeof(pal_param_bta2dp_t));
+            ret = pal_set_param(PAL_PARAM_ID_BT_A2DP_SUSPENDED, (void *)&param_bt_a2dp,
+                                sizeof(pal_param_bta2dp_t));
+        }
     } else if (session_type == LE_AUDIO_HARDWARE_OFFLOAD_DECODING_DATAPATH) {
         if (state == 0) {
+            std::unique_lock<std::mutex> guard(reconfig_wait_mutex_);
             param_bt_a2dp.a2dp_capture_suspended = true;
+            param_bt_a2dp.dev_id = PAL_DEVICE_IN_BLUETOOTH_BLE;
+
+            ret = pal_set_param(PAL_PARAM_ID_BT_A2DP_CAPTURE_SUSPENDED, (void *)&param_bt_a2dp,
+                                sizeof(pal_param_bta2dp_t));
         } else if (state == 1) {
             param_bt_a2dp.a2dp_capture_suspended = false;
+            param_bt_a2dp.dev_id = PAL_DEVICE_IN_BLUETOOTH_BLE;
+
+            ret = pal_set_param(PAL_PARAM_ID_BT_A2DP_CAPTURE_SUSPENDED, (void *)&param_bt_a2dp,
+                                sizeof(pal_param_bta2dp_t));
         }
-        param_bt_a2dp.dev_id = PAL_DEVICE_IN_BLUETOOTH_BLE;
-
-        ret = pal_set_param(PAL_PARAM_ID_BT_A2DP_CAPTURE_SUSPENDED, (void *)&param_bt_a2dp,
-                            sizeof(pal_param_bta2dp_t));
     }
-
+    AHAL_VERBOSE("reconfig_cb exit");
     return ret;
 }
 