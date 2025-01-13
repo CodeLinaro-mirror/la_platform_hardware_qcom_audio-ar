cc98b1af2ea9cc483c97380cae5100a0a652a204
Jaideep Sharma <quic_jaidshar@quicinc.com>
2025-01-13 18:12:34 +0530

    hal: Use new pal address field for external devices
    Pass device address for external devices in new pal fields.
    
    Change-Id: I547e815ed3c048b2044754332dfdfc69079a372f

--- hal/core/platform/include/qti-audio-core/PlatformUtils.h
+++ hal/core/platform/include/qti-audio-core/PlatformUtils.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2023-2025 Qualcomm Innovation Center, Inc. All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
@@ -12,8 +12,8 @@
 
 namespace qti::audio::core {
 
-using ::aidl::android::media::audio::common::AudioProfile;
 using ::aidl::android::media::audio::common::AudioChannelLayout;
+using ::aidl::android::media::audio::common::AudioProfile;
 
 constexpr size_t getNearestMultiple(size_t num, size_t multiplier) {
     size_t remainder = 0;
@@ -65,6 +65,16 @@ void setPalDeviceCustomKey(pal_device& palDevice, const std::string& customKey) 
 
 std::vector<uint8_t> makePalVolumes(std::vector<float> const& volumes) noexcept;
 
+/**
+ * @brief Validates the provided AudioDeviceAddress and creates a new PAL device address.
+ * @param address A constant reference to an AudioDeviceAddress object.
+ * @param palDevice A valid constant pointer to a pal_device object.
+ *
+ * @return true if the address is valid and a new PAL device address is created successfully.
+ * @return false otherwise.
+ */
+bool makePalDeviceAddress(const aidl::android::media::audio::common::AudioDeviceAddress& address,
+                          pal_device* const palDevice);
 /*
 * validates if the playback rate parameters are valid
 */
