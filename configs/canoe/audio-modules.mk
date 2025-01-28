696f3f28a6ca9cace84c62534daaba156a09f138
Lakshmi Prahitha Pendam <quic_pendam@quicinc.com>
2025-01-28 19:32:30 +0530

    hal: register paleventnotifier service
    Change-Id: I44e55cd4811c0e5e9a46244aece8790af669708a

--- configs/canoe/audio-modules.mk
+++ configs/canoe/audio-modules.mk
@@ -35,6 +35,7 @@ AUDIO_PAL += libhfp_pal
 #PAL Service
 AUDIO_PAL += libpalclient
 AUDIO_PAL += libpalipcservice
+AUDIO_PAL += libpaleventnotifier
 #PAL Stream Modules
 AUDIO_PAL += libstream_compress
 AUDIO_PAL += libstream_incall
