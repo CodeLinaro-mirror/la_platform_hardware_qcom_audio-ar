eb5d3a85ceed3ee37e174713547ba285bc98b9cb
Vijay Kumar Maddula <quic_vmaddula@quicinc.com>
2025-02-21 12:14:05 +0530

    hal: fixed custom config key addition
    - Fixed custom config addition to prevent
      overwriting to existing custom configs.
    
    Change-Id: I0ce3a408e4ce3780d7e5edef753d87e137eba007

--- hal/AudioStream.cpp
+++ hal/AudioStream.cpp
@@ -5824,7 +5824,7 @@ StreamInPrimary::StreamInPrimary(audio_io_handle_t handle,
             uint8_t channels =
                 audio_channel_count_from_in_mask(config_.channel_mask);
             if (channels == 2) {
-                strlcpy(mPalInDevice[i].custom_config.custom_key, "dual-mic-true-stereo",
+                strlcat(mPalInDevice[i].custom_config.custom_key, "dual-mic-true-stereo",
                         sizeof(mPalInDevice[i].custom_config.custom_key));
                 AHAL_INFO("Setting custom key as %s", mPalInDevice[i].custom_config.custom_key);
             }
