f6dcbe7ca12c2b3260d59e89fdb126805bccd58c
Nagarjuna Paladugu <quic_npaladug@quicinc.com>
2025-02-27 20:26:10 +0530

    AHAL : Volume control for audio clips across all channels
    Change-Id: I92141ff8fa926ca8b54a8376a826413f6a65b796
    Signed-off-by: Nagarjuna Paladugu <quic_npaladug@quicinc.com>

--- hal/core/ModulePrimary.cpp
+++ hal/core/ModulePrimary.cpp
@@ -73,7 +73,7 @@ using ::aidl::android::hardware::audio::common::isBitPositionFlagSet;
 using ::aidl::android::hardware::audio::common::isValidAudioMode;
 using ::aidl::android::hardware::audio::common::SinkMetadata;
 using ::aidl::android::hardware::audio::common::SourceMetadata;
-
+using ::aidl::android::hardware::audio::common::getChannelCount;
 using ::aidl::android::hardware::audio::core::AudioPatch;
 using ::aidl::android::hardware::audio::core::AudioRoute;
 using ::aidl::android::hardware::audio::core::IStreamIn;
@@ -134,12 +134,18 @@ ndk::ScopedAStatus qti::audio::core::ModulePrimary::setAudioPortConfig(const ::a
     LOG(DEBUG) << "the module list is not empty";
     auto iter = list.begin();
     auto route_portid = route->sourcePortIds.begin();
-    for (iter; (iter != list.end()&& route_portid!=route->sourcePortIds.end()); iter++) {
-       auto outIter = iter->lock();
+    for (iter; route_portid!=route->sourcePortIds.end(); iter++) {
+        if(iter == list.end()) {
+           iter = list.begin();
+           LOG(DEBUG) << __func__ << "port not found " << *route_portid;
+           route_portid++;
+        }
+        auto outIter = iter->lock();
         if (outIter) {
             auto &mcontext = (*outIter).getStreamContext();
             auto &list_audioportconfig = mcontext.getMixPortConfig();
             list_id = list_audioportconfig.portId;
+            int no_of_channels = (int)getChannelCount(list_audioportconfig.channelMask.value());
             float volume;
             if (list_id == (*route_portid)) {
                 std::vector<float> vol;
@@ -160,8 +166,10 @@ ndk::ScopedAStatus qti::audio::core::ModulePrimary::setAudioPortConfig(const ::a
                 else {
                     volume = (static_cast<float>(in_requested.gain->values[0]));
                 }
-                vol.push_back(volume);
-                vol.push_back(volume);
+                int iter_channel;
+                for(iter_channel = 0; iter_channel<no_of_channels; iter_channel++) {
+                    vol.push_back(volume);
+                }
                 LOG(DEBUG) << "gain is:" << volume;
                 LOG(DEBUG) << "volume is:" << vol[0];
                 (std::static_pointer_cast<::qti::audio::core::StreamOutPrimary>(outIter))->setHwVolume(vol);
