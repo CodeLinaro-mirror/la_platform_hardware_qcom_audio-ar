0e67c04e109bc51259d6859834b8134c38b0bd4f
Nagarjuna Paladugu <quic_npaladug@quicinc.com>
2025-04-14 11:21:50 +0530

    AHAL: Fix for multiple attenuation leveling calls during       comoressed_offload playback.
    Change-Id: I3c30cb285531af526e35040d55b5f7b844467307

--- hal/core/ModulePrimary.cpp
+++ hal/core/ModulePrimary.cpp
@@ -107,7 +107,7 @@ namespace qti::audio::core {
 
 std::vector<std::weak_ptr<::qti::audio::core::StreamOut>> ModulePrimary::mStreamsOut;
 std::vector<std::weak_ptr<::qti::audio::core::StreamIn>> ModulePrimary::mStreamsIn;
-std::string qti::audio::core::ModulePrimary::globalAudioSource;
+std::string qti::audio::core::ModulePrimary::globalAudioSource = "other";
 
 #ifdef ENABLE_QCOM_AMPERE_AUDIO
 std::unordered_map<std::string, FocusSession> ModulePrimary::mActiveFocusDevices;
