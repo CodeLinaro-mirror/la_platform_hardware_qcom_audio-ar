39b36c2c67aeee6f13fd7dd6a2714b5871803814
Pradeep Chodavarapu <quic_pchodava@quicinc.com>
2025-05-12 10:44:45 +0530

    audio-hal : Added AUTO related BUS in xml's
    Added auto specific input and output devices for
    BUS audio routing support in gen4_au devices
    
    Change-Id: I6038b716f3468d4b148bfbd9eb5e73f86e7a86ac
    Signed-off-by: Raghu Ballappa Bankapur <rbankapu@qti.qualcomm.com>

--- configs/gen4_au/audio-modules.mk
+++ configs/gen4_au/audio-modules.mk
@@ -34,6 +34,8 @@ AUDIO_PAL += catf
 AUDIO_PAL += PalTest
 AUDIO_PAL += libaudiochargerlistener
 AUDIO_PAL += libhfp_pal
+AUDIO_PAL += lib_default_plugin_controls
+AUDIO_PAL += libautohal_pal
 #PAL Service
 AUDIO_PAL += libpalclient
 AUDIO_PAL += libpalipcservice
