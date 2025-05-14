9ef0f996f2ffeb324224d89b6ebe20ebf07f426d
Afroza Nasrin <sufiafro@qti.qualcomm.com>
2025-05-14 22:24:30 +0530

    hal: configs: Add support to copy alor xmls
    Add support to copy alor related xmls to proper
    device_sku location
    
    Change-Id: I00e56663ce7fb7b7a99535bda1ea1dd99b52a4ae
    (cherry picked from commit 582cc0719bd9eef4e52f180461faf446ce8dee3c)

--- configs/canoe/canoe.mk
+++ configs/canoe/canoe.mk
@@ -166,6 +166,62 @@ PRODUCT_COPY_FILES += \
     $(TOPDIR)vendor/qcom/opensource/audio-hal/primary-hal/configs/common/codec2/service/1.0/c2audio.vendor.ext-arm64.policy:vendor/etc/seccomp_policy/c2audio.vendor.ext-arm64.policy
 PRODUCT_COPY_FILES += \
     $(CONFIG_HAL_SRC_DIR)/vendor_audio_interfaces.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/vendor_audio_interfaces.xml
+   
+# Audio configuration xml's related to Molokai
+QCV_FAMILY_SKUS := alor
+DEVICE_SKU := alor
+UV_WRAPPER2 := true
+
+CONFIG_PAL_SRC_DIR := vendor/qcom/opensource/pal/configs/canoe
+CONFIG_HAL_SRC_DIR := vendor/qcom/opensource/audio-hal/primary-hal/configs/canoe
+CONFIG_SKU_OUT_DIR := $(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_$(DEVICE_SKU)
+
+PRODUCT_COPY_FILES += \
+    $(CONFIG_HAL_SRC_DIR)/audio_effects.conf:$(CONFIG_SKU_OUT_DIR)/audio_effects.conf \
+    $(CONFIG_HAL_SRC_DIR)/audio_effects.xml:$(CONFIG_SKU_OUT_DIR)/audio_effects.xml \
+    $(CONFIG_HAL_SRC_DIR)/audio_effects_config.xml:$(CONFIG_SKU_OUT_DIR)/audio_effects_config.xml \
+    $(CONFIG_HAL_SRC_DIR)/microphone_characteristics.xml:$(TARGET_COPY_OUT_VENDOR)/etc/microphone_characteristics.xml \
+    $(CONFIG_PAL_SRC_DIR)/card-defs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/card-defs.xml \
+    $(CONFIG_PAL_SRC_DIR)/mixer_paths_alor_mtp_wcd9378.xml:$(CONFIG_SKU_OUT_DIR)/mixer_paths_alor_mtp_wcd9378_snd_card.xml \
+    $(CONFIG_PAL_SRC_DIR)/mixer_paths_alor_mtp_wcd939x.xml:$(CONFIG_SKU_OUT_DIR)/mixer_paths_alor_mtp_wcd939x_snd_card.xml \
+    $(CONFIG_PAL_SRC_DIR)/mixer_paths_alor_qrd.xml:$(CONFIG_SKU_OUT_DIR)/mixer_paths_alor_qrd_snd_card.xml \
+    $(CONFIG_PAL_SRC_DIR)/mixer_paths_alor_cdp.xml:$(CONFIG_SKU_OUT_DIR)/mixer_paths_alor_cdp_snd_card.xml \
+    $(CONFIG_PAL_SRC_DIR)/resourcemanager_alor_mtp_wcd9378.xml:$(CONFIG_SKU_OUT_DIR)/resourcemanager_alor_mtp_wcd9378.xml \
+    $(CONFIG_PAL_SRC_DIR)/resourcemanager_alor_mtp_wcd939x.xml:$(CONFIG_SKU_OUT_DIR)/resourcemanager_alor_mtp_wcd939x.xml \
+    $(CONFIG_PAL_SRC_DIR)/resourcemanager_alor_cdp.xml:$(CONFIG_SKU_OUT_DIR)/resourcemanager_alor_cdp.xml	 \
+    $(CONFIG_PAL_SRC_DIR)/resourcemanager_alor_qrd.xml:$(CONFIG_SKU_OUT_DIR)/resourcemanager_alor_qrd.xml	 \
+    $(CONFIG_PAL_SRC_DIR)/usecaseKvManager.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usecaseKvManager.xml \
+    $(CONFIG_PAL_SRC_DIR)/Hapticsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/Hapticsconfig.xml \
+    $(CONFIG_PAL_SRC_DIR)/plugin_manager.xml:$(CONFIG_SKU_OUT_DIR)/plugin_manager.xml \
+    vendor/qcom/opensource/audio-hal/primary-hal/configs/common/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml \
+    frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
+    frameworks/native/data/etc/android.hardware.sensor.dynamic.head_tracker.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_$(DEVICE_SKU)/android.hardware.sensor.dynamic.head_tracker.xml
+# Copy AudioEffects config
+PRODUCT_COPY_FILES += \
+    hardware/interfaces/audio/aidl/default/audio_effects_config.xml:$(CONFIG_SKU_OUT_DIR)/audio_effects_config_stub.xml
+
+# Copy Quasar config
+PRODUCT_COPY_FILES += \
+    $(CONFIG_HAL_SRC_DIR)/quasar_config.xml:$(CONFIG_SKU_OUT_DIR)/quasar_config.xml
+
+#XML Audio configuration files
+ifneq ($(TARGET_USES_AOSP_FOR_AUDIO), true)
+PRODUCT_COPY_FILES += \
+    $(CONFIG_HAL_SRC_DIR)/audio_policy_configuration.xml:$(CONFIG_SKU_OUT_DIR)/audio_policy_configuration.xml
+
+#Audio configuration xml's common to canoe family
+PRODUCT_COPY_FILES += \
+$(foreach DEVICE_SKU, $(QCV_FAMILY_SKUS), \
+    $(CONFIG_HAL_SRC_DIR)/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_$(DEVICE_SKU)_qssi/audio_policy_configuration.xml)
+
+PRODUCT_COPY_FILES += \
+    $(CONFIG_HAL_SRC_DIR)/audio_module_config_primary.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_module_config_primary.xml
+endif
+
+# XML config file for memory logger
+PRODUCT_COPY_FILES += $(TOPDIR)vendor/qcom/opensource/audio-hal/primary-hal/configs/$(DEVICE_SKU)/mem_logger_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mem_logger_config.xml
+
 
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 PRODUCT_COPY_FILES += \
