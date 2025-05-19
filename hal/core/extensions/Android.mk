SunOS-Project/android_hardware_qcom_audio-ar

AHAL: use HFP device if HFP server and client property is enabled
Add HFP server and client property to enable/disable feature.

Use HFP device for HFP call client connection which will connect
target device and remote phone, if HFP server and client property
is enabled.

Change-Id: I368590aef0c39ab989261eed17a3b1b0a32e5e2e

hal/core/extensions/Android.mk
@@ -51,6 +51,10 @@ ifeq ($(TARGET_BOARD_AUTO),true)
   LOCAL_CFLAGS += -DPLATFORM_AUTO
 endif
 
+ifeq ($(strip $(AUDIO_FEATURE_ENABLED_HFP_SERVER_CLINET)), true)
+LOCAL_CFLAGS           += -DHFP_SERVER_CLIENT_ENABLED
+endif
+
 LOCAL_SRC_FILES:= Hfp.cpp
 
 LOCAL_CFLAGS += \

CodeLinaro-mirror/la_platform_hardware_qcom_audio-ar

(cherry picked from commit 63db57dfe1c1f198dccc6610a8a7d011db351515)
(cherry picked from commit 965789948527fded2d1f6266e1b43c11a621ccad)

hal/core/extensions/Android.mk
@@ -47,6 +47,10 @@ ifeq ($(TARGET_BOARD_AUTO),true)
   LOCAL_CFLAGS += -DPLATFORM_AUTO
 endif
 
+ifeq ($(strip $(AUDIO_FEATURE_ENABLED_HFP_SERVER_CLIENT)), true)
+LOCAL_CFLAGS           += -DHFP_SERVER_CLIENT_ENABLED
+endif
+
 LOCAL_SRC_FILES:= Hfp.cpp
 
 LOCAL_CFLAGS += \