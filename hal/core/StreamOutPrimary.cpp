40fc71b7f857f08a9f69f314711942818225d4b1
Venu Gopal Atchyutanna <vatchyut@qti.qualcomm.com>
2025-06-09 20:22:25 +0530

    AHAL: Fix for Mute/Unmute for compress offload/pcm offload graphs
    - Fetching the mute status from PRIMARY_PLAYBACK and using same
      state for compress offload and pcm offload usecases
    
    Change-Id: I538c075e0af6372979e78863bd19bd5880177d6a

--- hal/core/StreamOutPrimary.cpp
+++ hal/core/StreamOutPrimary.cpp
@@ -1156,7 +1156,9 @@ void StreamOutPrimary::configure() {
                     std::vector<float> Volumes;
                     int iter_channel;
                     int no_of_channels = mVolumes.size();
-                    if (streamOutPrimary->isStreamOutPrimary()) {
+                    if (streamOutPrimary->isStreamOutPrimary() || streamOutPrimary->isStreamOutMedia()) {
+                        /* Get mute status and volumes from PRIMARY_PLAYBACK or MEDIA_PLAYBACK */
+                        mMuted = streamOutPrimary->mMuted;
                         streamOutPrimary->getHwVolume(&Volumes);
                         for(iter_channel = 0; iter_channel < no_of_channels; iter_channel++) {
                             mVolumes[iter_channel] = Volumes[0];
