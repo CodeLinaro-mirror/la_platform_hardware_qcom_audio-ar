20f1319c832a6329095b480ff26c457a42f1f264
Vasantha Kumar Raju Angappan <quic_vangappa@quicinc.com>
2025-09-03 12:37:31 +0530

    AHAL: Add getParameter for mic_occlusion_info
    - add get parameter support for AUDIO_PARAMETER_KEY_MIC_OCCLUSION_INFO.
    
    Reference Change-Id: Ida864c967cc99bbffc980b7a19188e5256c55d11
    
    Change-Id: I72c1958c2318779414b7f01fa5fede3407443ab2
    (cherry picked from commit d9d3ff87f46d032359f07382e32cc963eab27bc5)

--- hal/core/ModulePrimary.cpp
+++ hal/core/ModulePrimary.cpp
@@ -15,8 +15,8 @@
  */
 
 /*
- * Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
- * Copyright (c) 2023-2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
@@ -720,6 +720,7 @@ ModulePrimary::SetParameterToFeatureMap ModulePrimary::fillSetParameterToFeature
                                  {Parameters::kFbspValiWaitTime, Feature::FTM},
                                  {Parameters::kFbspValiValiTime, Feature::FTM},
                                  {Parameters::kTriggerSpeakerCall, Feature::FTM},
+                                 {Parameters::kMicOcclusionParam, Feature::MICOCCLUSION},
                                  {Parameters::kWfdChannelMap, Feature::WFD},
                                  {Parameters::kWfdIPAsProxyDevConnected, Feature::WFD},
                                  {Parameters::kProxyRecordFMQSize, Feature::WFD},
@@ -984,6 +985,30 @@ std::vector<VendorParameter> ModulePrimary::onGetFTMParameters(
     return results;
 }
 
+std::vector<VendorParameter> ModulePrimary::onGetMicOcclusionParameters(
+        const std::vector<std::string>& ids) {
+    std::vector<VendorParameter> results{};
+    // Only valid IDs are added to results; unknown IDs are logged and skipped.
+    for (const auto& id : ids) {
+        if (id == Parameters::kMicOcclusionParam) {
+            VendorParameter param;
+            VString parcel;
+            param.id = id;
+            const auto& micocclsionresult = mPlatform.getMicocclusionparameter();
+            if (micocclsionresult) {
+                parcel.value = micocclsionresult.value();
+            } else {
+                parcel.value = "false";
+            }
+            setParameter(parcel, param);
+            results.push_back(param);
+        } else {
+            LOG(ERROR) << __func__ << ": unknown parameter in MicOcclusion feature. id:" << id;
+        }
+    }
+    return results;
+}
+
 // static
 ModulePrimary::GetParameterToFeatureMap ModulePrimary::fillGetParameterToFeatureMap() {
     GetParameterToFeatureMap map{{Parameters::kHdrRecord, Feature::HDR},
@@ -1001,6 +1026,7 @@ ModulePrimary::GetParameterToFeatureMap ModulePrimary::fillGetParameterToFeature
                                  {Parameters::kWfdIPAsProxyDevConnected, Feature::WFD},
                                  {Parameters::kFTMParam, Feature::FTM},
                                  {Parameters::kFTMSPKRParam, Feature::FTM},
+                                 {Parameters::kMicOcclusionParam, Feature::MICOCCLUSION},
                                  {Parameters::kFMStatus, Feature::AUDIOEXTENSION}};
     return map;
 }
@@ -1013,6 +1039,7 @@ ModulePrimary::FeatureToGetHandlerMap ModulePrimary::fillFeatureToGetHandlerMap(
                                {Feature::WFD, &ModulePrimary::onGetWFDParameters},
                                {Feature::FTM, &ModulePrimary::onGetFTMParameters},
                                {Feature::AUDIOEXTENSION, &ModulePrimary::onGetAudioExtnParams},
+                               {Feature::MICOCCLUSION, &ModulePrimary::onGetMicOcclusionParameters},
                                {Feature::GENERIC, &ModulePrimary::onGetGenericParams}};
     return map;
 }
