3435e9c7a5276667005453b5402eaa7995274233
chxu <chxu@qti.qualcomm.com>
2026-03-16 19:32:49 +0800

    configs:yupik: add xmls for yupik rb3
    add mixer_paths_lahaina_yupikidprb3.xml
    add resourcemanager_lahaina_yupikidprb3.xml in copy_files
    
    Change-Id: Ib00b3677e791ea0f403ce765858cce6bf9b3c478

--- configs/lahaina/lahaina.mk
+++ configs/lahaina/lahaina.mk
@@ -184,8 +184,10 @@ PRODUCT_COPY_FILES += \
     $(CONFIG_HAL_SRC_DIR)/microphone_characteristics.xml:$(TARGET_COPY_OUT_VENDOR)/etc/microphone_characteristics.xml \
     $(CONFIG_HAL_SRC_DIR)/mixer_paths_lahaina_yupikqrd.xml:$(CONFIG_SKU_OUT_DIR)/mixer_paths_lahaina_yupikqrd.xml \
     $(CONFIG_HAL_SRC_DIR)/mixer_paths_lahaina_yupikidp.xml:$(CONFIG_SKU_OUT_DIR)/mixer_paths_lahaina_yupikidp.xml \
+    $(CONFIG_HAL_SRC_DIR)/mixer_paths_lahaina_yupikidprb3.xml:$(CONFIG_SKU_OUT_DIR)/mixer_paths_lahaina_yupikidprb3.xml \
     $(CONFIG_PAL_SRC_DIR)/resourcemanager_lahaina_yupikidp.xml:$(CONFIG_SKU_OUT_DIR)/resourcemanager_lahaina_yupikidp.xml \
     $(CONFIG_PAL_SRC_DIR)/resourcemanager_lahaina_yupikqrd.xml:$(CONFIG_SKU_OUT_DIR)/resourcemanager_lahaina_yupikqrd.xml \
+    $(CONFIG_PAL_SRC_DIR)/resourcemanager_lahaina_yupikidprb3.xml:$(CONFIG_SKU_OUT_DIR)/resourcemanager_lahaina_yupikidprb3.xml \
     $(CONFIG_PAL_SRC_DIR)/usecaseKvManager.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usecaseKvManager.xml \
     $(CONFIG_PAL_SRC_DIR)/plugin_manager.xml:$(CONFIG_SKU_OUT_DIR)/plugin_manager.xml \
     vendor/qcom/opensource/audio-hal/primary-hal/configs/common/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml \
