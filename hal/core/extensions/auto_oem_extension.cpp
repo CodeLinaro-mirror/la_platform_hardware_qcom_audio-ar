1a9dd27ae41a1eccf0472531b384b312c654f422
Dheeraj Kumar Karanam <lkaranam@qti.qualcomm.com>
2026-04-02 17:32:19 +0530

    Updated Powerpolicy handling for SDVC
    Change-Id: I7618b825159c9ac10a9fe208a44be3adc6acc4dc
    (cherry picked from commit b64af0f8e59bfaa1972b75922f6eb826a8e7d9a5)

--- hal/core/extensions/auto_oem_extension.cpp
+++ hal/core/extensions/auto_oem_extension.cpp
@@ -88,6 +88,8 @@
 
 #define FADER_BALANCE_MIN_INPUT -1.0f
 #define FADER_BALANCE_MAX_INPUT 1.0f
+#define POWER_POLICY_ENABLE 1U
+#define POWER_POLICY_DISABLE 0U
 
 #define FADER_BALANCE_SCALE 5.0f
 
@@ -302,7 +304,7 @@ void update_sdvc(param_type2_t *params) {
             pal_param->param_size = sizeof(param_type2_t);
             pal_param->data = (void *)params;
             LOG(VERBOSE) << __func__ << ": Successfully created pal_param";
-            if (gs_powerpolicy) {
+            if (POWER_POLICY_ENABLE == gs_powerpolicy) {
                 if (gsp_palHandle == NULL) {
                     open_stream();
                 }
@@ -358,12 +360,12 @@ void set_powerpolicy(struct str_parms *parms)
 
         // Power Policy is enabled
         if (result) {
-            gs_powerpolicy = 1U;
+            gs_powerpolicy = POWER_POLICY_ENABLE;
             // Open Graph and gsp_palHandle
             open_stream();
         } else {
             // Close Graph and update gsp_palHandle to NULL
-            gs_powerpolicy = 0U;
+            gs_powerpolicy = POWER_POLICY_DISABLE;
             close_stream();
         }
     }
@@ -483,7 +485,7 @@ void update_fan_speed_pal_param(param_type2_t *params) {
             pal_param->data = (void *)params;
             LOG(DEBUG) << __func__ << ": Successfully created pal_param";
 
-            if (gs_powerpolicy) {
+            if (POWER_POLICY_ENABLE == gs_powerpolicy) {
                 if (gsp_palHandle == NULL) {
                     open_stream();
                 }
@@ -526,7 +528,7 @@ void update_vehicle_speed_pal_param(param_type2_t *params) {
             pal_param->param_size = sizeof(param_type2_t);
             pal_param->data = (void *)params;
             LOG(VERBOSE) << __func__ << ": Successfully created pal_param";
-            if (gs_powerpolicy) {
+            if (POWER_POLICY_ENABLE == gs_powerpolicy) {
                 if (gsp_palHandle == NULL) {
                     open_stream();
                 }
