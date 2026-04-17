d791b68cff3799fa269577d3183cfcfb0f2cd208
Kiran Tukaram Kadam <kkadam@qti.qualcomm.com>
2026-04-17 14:43:49 +0530

    audio-hal: configs: hamoa_la:  Enable MSPP support by default
    Enable MSPP by default for hamoa_la audio configuration.
    
    Change-Id: If92924ebaf5d07477b01ae6435c703dc885b49ff

--- configs/hamoa_la/hamoa_la.mk
+++ configs/hamoa_la/hamoa_la.mk
@@ -163,6 +163,10 @@ vendor.audio.offload.multiple.enabled=false
 PRODUCT_PROPERTY_OVERRIDES += \
 vendor.audio.flac.sw.decoder.24bit=true
 
+#Enable mspp by default
+PRODUCT_PROPERTY_OVERRIDES += \
+vendor.audio.mspp.enable=true
+
 #split a2dp DSP supported encoder list
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac
