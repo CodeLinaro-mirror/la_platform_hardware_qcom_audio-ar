9c0a90a05822b5e00d59afc9a7bb8e4cd5cb5b7e
Yi Lu <ylu7@qti.qualcomm.com>
2026-05-21 18:15:24 +0800

    hal: add FNN model file into product package
    Add updated 3mic-FNN (FV13) model files targeted for RB3GEN2
    into product package, so that FNN model files will be installed
    in vendor image.
    
    Change-Id: I9a22c566c7c6fd7e913ac52410215694b51714d7

--- configs/lahaina/lahaina.mk
+++ configs/lahaina/lahaina.mk
@@ -153,6 +153,7 @@ PRODUCT_PACKAGES += fai__2.0.0_0.1__3.0.0_0.0__eai_1.36_enpu2.pmd
 PRODUCT_PACKAGES += fai__2.7.2_0.0__3.0.0_0.0__eai_1.36_enpu2.pmd
 PRODUCT_PACKAGES += fai__2.7.20_0.0__3.0.0_0.0__eai_1.36_enpu2.pmd
 PRODUCT_PACKAGES += fai__3.0.0_0.0__eai_1.36_enpu2.pmd
+PRODUCT_PACKAGES += fai__4.8.8_0.0__3.0.0_0.0__3.1.2_0.0__3.2.0_0.1__eai_1.10.pmd
 PRODUCT_PACKAGES += libfmpal
 PRODUCT_PACKAGES += event.eai
 PRODUCT_PACKAGES += music.eai
