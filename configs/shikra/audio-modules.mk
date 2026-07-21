3db79e4120a64e03dbf5b961f8dd7496b6d9850d
Samir Yadav <samiyada@qti.qualcomm.com>
2026-07-21 20:12:47 +0530

    configs: shikra: add fluence model for dual mic support
    Add Fluence model entry for dual mic support.
    
    Change-Id: I43296b2065f2a00a7cefb301f1de55893f49e618
    (cherry picked from commit 5a70daf4d75f4354c869ada8530db249eca5b374)

--- configs/shikra/audio-modules.mk
+++ configs/shikra/audio-modules.mk
@@ -96,6 +96,7 @@ AUDIO_MODULES += libvui_utils
 
 AUDIO_MODULES += ffv__7.1.1_0.1__eai3.19_adsp.eai
 AUDIO_MODULES += fai__4.14.0_0.0__3.0.0_0.0__3.1.2_0.0__3.2.0_0.1__eai_3.18_adsp.pmd
+AUDIO_MODULES += fai__4.14.0_1.0__3.0.0_0.0__3.1.2_0.0__3.2.0_0.1__eai_3.18_adsp.pmd
 
 AUDIO_MODULES += hotword_plugin
 AUDIO_MODULES += customva_plugin
