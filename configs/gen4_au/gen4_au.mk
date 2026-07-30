a75257bb434de5b5e045da66067bbfe656257da6
Roja Rani Yarubandi <ryaruban@qti.qualcomm.com>
2026-07-30 18:26:38 +0530

    Revert "AHAL: Disable Qcom parser 3GP"
    This reverts commit 66f865f9e236738a4f891b052e72b36e0b1e8995.
    
    Change-Id: I983fbab723ad4b8bbfe0d0663f1dbebbab0e7a61
    Signed-off-by: Roja Rani Yarubandi <ryaruban@qti.qualcomm.com>
    (cherry picked from commit 1d5bd4baac1c7067586700993f98c5e2dd2bddf2)

--- configs/gen4_au/gen4_au.mk
+++ configs/gen4_au/gen4_au.mk
@@ -112,9 +112,9 @@ AUDIO_FEATURE_ENABLED_AUDIO_CONTROL_HAL := true
 AUDIO_FEATURE_ENABLED_AUDIO_PARSERS := true
 
 #enable qcom parsers for WMA/APE/FLAC/ALAC
-# disable 3GP qcom parser for APV CTS TCs
+# disable 3GP qcom parser
 PRODUCT_PROPERTY_OVERRIDES += \
-vendor.mm.target.enable.qcom_parser=655376
+vendor.mm.target.enable.qcom_parser=655632
 
 
 ifneq ($(ENABLE_HYP),true)
